0f7988da2df65d2b0302356aef99635ba1203012
silancMariah <155994646+silancMariah@users.noreply.github.com>
2025-11-10 01:20:20 +0100

    delat upp filerna

---
+++ Pass1/jorden_solen.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import time
+import turtle 
+
+
+screen = turtle.Screen()
+screen.setup(width=800, height=600)
+screen.title("Min Jord")
+
+earth = turtle.Turtle() 
+earth.shape("circle") #Enkel cirkel
+earth.turtlesize(10) #jordens storlek
+earth.color("blue")
+
+# ------ Rita solen här: ----- #
+sun = turtle.Turtle()
+
+
+# ---------------------------- #
+
+
+
+# ------ Skriv en text här: ----- #
+text = turtle.Turtle()
+
+
+# ------------------------------- #text.penup()
+
+def update_color(temp):
+     if temp > 20:
+        earth.color("red") 
+        # ----- text -----#
+     elif temp < 10:
+        earth.color("lightblue")
+        # ----- text -----#
+     else:
+        earth.color("green")
+        # ----- text -----#
+        
+update_color(21)
+
+
+def spin():
+    earth.right(90)
+    spin()
+    
+spin()
+
+
+turtle.done()
+
+
